596ce505aa81b14e507de96b8aae357f3afdae08
HiiYL <yonglian146@gmail.com>
2016-08-26 15:48:23 +0800

    Working aesthetics evaluation

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, redirect, url_for, render_template,send_from_directory
+from flask import g
 from werkzeug import secure_filename
 
 import os
-from vgg import *
+from ava import *
 import pandas as pd
 import cv2, numpy as np
+from scipy import ndimage, misc
 
 UPLOAD_FOLDER = 'uploads/'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
@@ -18,25 +20,19 @@ def allowed_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def root():
     if request.method == 'POST':
-      names = pd.read_table("synset_words.txt", header = None,names=["names"])
       file = request.files['file']
       if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         file.seek(0)
 
-        raw_image = cv2.imdecode(np.fromstring(file.read(), np.uint8), cv2.CV_LOAD_IMAGE_UNCHANGED)
-        im = cv2.resize(raw_image, (224, 224)).astype(np.float32)
-        im[:,:,0] -= 103.939
-        im[:,:,1] -= 116.779
-        im[:,:,2] -= 123.68
-        im = im.transpose((2,0,1))
-        im = np.expand_dims(im, axis=0)
-
+        image = ndimage.imread(file, mode="RGB")
+        image_resized = misc.imresize(image, (224, 224)).T
+        im = np.expand_dims(image_resized,axis=0)
 
         out = model.predict(im)
-        object_class = str(names.loc[np.argmax(out)]["names"])[10:]
-        return render_template('response.html',object_class = object_class, filename=filename)
+
+        return render_template('response.html',object_class = out, filename=filename)
     return app.send_static_file('index.html')
 
 
@@ -47,8 +43,8 @@ def uploaded_file(filename):
 
 if __name__ == "__main__":
     # app.debug = True
-    model = VGG_16('vgg16_weights.h5')
+    model = VGG_19('ava_vgg_19_1.0_5.h5')
     sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(optimizer=sgd, loss='categorical_crossentropy')
-    app.run(host='0.0.0.0')
+    app.run()
     # app.run()
